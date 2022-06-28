5e5aaff65255306780deb35aa8bfd7945ee3d854
Chris Niebuhr <48517262+ChrisNiebuhrMSFT@users.noreply.github.com>
2022-06-28 15:51:30 +0200

    Update Install-Sysinternals.ps1
    Added Synopsis and Disclaimer

--- Install-Sysinternals.ps1
+++ Install-Sysinternals.ps1
@@ -1,3 +1,39 @@
+<#
+Disclaimer
+
+This sample script is not supported under any Microsoft standard support program or service. 
+The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims 
+all implied warranties including, without limitation, any implied warranties of merchantability 
+or of fitness for a particular purpose. The entire risk arising out of the use or performance 
+of the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, 
+or anyone else involved in the creation, production, or delivery of the scripts be liable for 
+any damages whatsoever (including, without limitation, damages for loss of business profits, 
+business interruption, loss of business information, or other pecuniary loss) arising out of the 
+use of or inability to use the sample scripts or documentation, even if Microsoft has been advised 
+of the possibility of such damages
+#>
+
+<#
+.SYNOPSIS
+  Installs or Updates the Sysinternals Tools on your System
+.DESCRIPTION
+  Installs or Updates the Sysinternals Tools on your System from the 
+  official Sysinternals Repository or optional from a given Fileshare, Folder or Webserver
+.LINK
+   https://github.com/ChrisNiebuhrMSFT/Install-Sysinternals
+.EXAMPLE
+  .\Install-Sysinternals.ps1 # Installs/Updates the current Sysinternals Version under C:\SysinternalSuite
+.EXAMPLE
+  .\Install-Sysinternals.ps1 -SourcePath \\Myfileshare\Sysinternals.zip # Installs/Updates Sysinternal Suite from a Custom Fileshare
+.EXAMPLE
+  .\Install-Sysinternals.ps1 -Force # Install/Updates Sysinternal Suite and closes any running Sysinternals Process which is actually running on the System without confirmation
+  #When the -Force Parameter is not specified and any running Sysinternal Process in running while updating the user will be propmpted to close the running Processes
+.NOTES
+  Runs with Windows PowerShell 5.x and PowerShell Core
+  Author: Microsoft - Chris Niebuhr
+  Date:   06/28/2022
+#>
+
 [CmdletBinding()]
 Param 
 (
